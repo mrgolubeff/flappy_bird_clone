b597a07ae6e5d3917baf47766c24d02f4b2a6db7
Vladimir Golubev <mrgolubeff@icloud.com>
2024-05-09 22:27:31 +0300

    Add pipes creation and movement logic

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var pipe_scene : PackedScene
+
 var game_running : bool
 var game_over : bool
 var scroll
@@ -14,6 +16,7 @@ const PIPE_RANGE : int = 200
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_window().size
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
 
 func new_game():
@@ -21,6 +24,8 @@ func new_game():
 	game_over = false
 	score = 0
 	scroll = 0
+	pipes.clear()
+	generate_pipes()
 	$Bird.reset()
 
 func _input(event):
@@ -37,6 +42,7 @@ func start_game():
 	game_running = true
 	$Bird.flying = true
 	$Bird.flap()
+	$PipeTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -45,3 +51,20 @@ func _process(delta):
 		if scroll >= screen_size.x:
 			scroll = 0
 		$Ground.position.x = -scroll
+		
+		for pipe in pipes:
+			pipe.position.x -= SCROLL_SPEED * delta
+
+func _on_pipe_timer_timeout():
+	generate_pipes()
+
+func generate_pipes():
+	var pipe = pipe_scene.instantiate()
+	pipe.position.x = screen_size.x + PIPE_DELAY
+	pipe.position.y = (screen_size.y - ground_height) / 2 + randi_range(-PIPE_RANGE, PIPE_RANGE)
+	pipe.hit.connect(bird_hit)
+	add_child(pipe)
+	pipes.append(pipe)
+
+func bird_hit():
+	pass
